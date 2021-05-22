f2a089e0b9dc99762b9b5f41e55168926a2bfba8
Olya <ilinskayaov@yandex.ru>
2021-05-23 00:35:56 +0300

    🗽
    1. Зай двоичная система счисления не так работает :D
    2. Ладно сдаюсь буду использовать только один таймер.
    3. Я еще комментарии могу написать.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,19 @@
-#include <iostream>
 #include "hal.h"
 
-using namespace std;
-
-const int T = 150;
+const int T = 150;//T по варианту задания
 
 int sw_num[] = {GPIO_PIN_4, GPIO_PIN_8, GPIO_PIN_10, GPIO_PIN_12};
 
-int getDelay(int x) {
-    return 500 + x * T;
+int getDelay(int x) {//расчет новой задержки в зависимости от переключателей
+    return (500 + x * T);
 }
 
-int getX() {
-    return (HAL_GPIO_ReadPin(GPIOE, sw_num[0]) * 1 + HAL_GPIO_ReadPin(GPIOE, sw_num[1]) * 2 +
-            HAL_GPIO_ReadPin(GPIOE, sw_num[2]) * 4 + HAL_GPIO_ReadPin(GPIOE, sw_num[3]) * 8);
+int getX() {//аргумент для расчета новой задержки
+    return (HAL_GPIO_ReadPin(GPIOE, sw_num[0]) * 8 + HAL_GPIO_ReadPin(GPIOE, sw_num[1]) * 4 +
+            HAL_GPIO_ReadPin(GPIOE, sw_num[2]) * 2 + HAL_GPIO_ReadPin(GPIOE, sw_num[3]) * 1);
 }
 
-int pattern[8][8] = {{0, 0, 0, 0, 0, 0, 1, 1},
+int pattern[8][8] = {{0, 0, 0, 0, 0, 0, 1, 1},//картинка
                      {0, 0, 0, 0, 0, 1, 1, 0},
                      {0, 0, 0, 0, 1, 1, 0, 0},
                      {0, 0, 0, 1, 1, 0, 0, 0},
@@ -29,16 +26,10 @@ int leds_num[] = {GPIO_PIN_3, GPIO_PIN_4, GPIO_PIN_5, GPIO_PIN_6, GPIO_PIN_8, GP
 int i = 0;
 
 void TIM6_IRQ_Handler() {
-    for (int j = 0; j < 8; j++) {
+    for (int j = 0; j < 8; j++) {//зажигание/гашение светодиодов в зависимости от кадра
         if (pattern[i][j]) {
             HAL_GPIO_WritePin(GPIOD, leds_num[j], GPIO_PIN_SET);
-        }
-    }
-}
-
-void TIM7_IRQ_Handler() {
-    for (int j = 0; j < 8; j++) {
-        if (pattern[i][j]) {
+        }else{
             HAL_GPIO_WritePin(GPIOD, leds_num[j], GPIO_PIN_RESET);
         }
     }
@@ -46,25 +37,16 @@ void TIM7_IRQ_Handler() {
     if (i == 8) {
         i = 0;
     }
+    WRITE_REG(TIM6_ARR, getDelay(getX()));//пересчет задержки
 }
 
 int umain() {
     i = 0;
     registerTIM6_IRQHandler(TIM6_IRQ_Handler);
-    registerTIM7_IRQHandler(TIM7_IRQ_Handler);
     __enable_irq();
     WRITE_REG(TIM6_ARR, 500);
     WRITE_REG(TIM6_DIER, TIM_DIER_UIE);
     WRITE_REG(TIM6_PSC, 0);
-    WRITE_REG(TIM7_ARR, 500);
-    WRITE_REG(TIM7_DIER, TIM_DIER_UIE);
-    WRITE_REG(TIM7_PSC, 1);
     WRITE_REG(TIM6_CR1, TIM_CR1_CEN);
-    WRITE_REG(TIM7_CR1, TIM_CR1_CEN);
-    while (true) {
-        HAL_Delay(1);
-        WRITE_REG(TIM6_ARR, getDelay(getX()));
-        WRITE_REG(TIM7_ARR, getDelay(getX()));
-    }
     return 0;
 }
